d5e4c7bbf0b5147bcadf7ef1b18c843599fdef3f
94BC37FFF5A4FE45A64A6AA25D960873 <94BC37FFF5A4FE45A64A6AA25D960873@i-search.com.cn>
2019-12-04 19:00:06 +0800

    控件使用

--- lib/page/android/android_page.dart
+++ lib/page/android/android_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/dao/home_articlelist_dao.dart';
 
 import 'package:flutter_app/page/android/activity_page.dart';
 
@@ -27,14 +26,13 @@ class AndroidState extends State<Android> {
                   child: Container(
                     margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
                     child: RaisedButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            new MaterialPageRoute(
-                                builder: (context) => new ActivityLearn()));
-                      },
-                      child: Text("activity"),
-                    ),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              new MaterialPageRoute(
+                                  builder: (context) => new ActivityLearn()));
+                        },
+                        child: Text("activity")),
                   ),
                 ),
                 Expanded(
@@ -42,14 +40,10 @@ class AndroidState extends State<Android> {
                   child: Container(
                     margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
                     child: RaisedButton(
-                      onPressed: () {
-                        HomeArticleListDao.fetch().then((homeArticleListModel) {
-                          print(homeArticleListModel.data.size);
-                        }).catchError((e) {
-                          print("e :" + e);
-                        });
-                      },
-                      child: Text("test"),
+                      onPressed: () {},
+                      child: Icon(
+                        Icons.home,
+                      ),
                     ),
                   ),
                 ),
@@ -62,10 +56,125 @@ class AndroidState extends State<Android> {
                       child: Text("test1"),
                     ),
                   ),
+                ),
+                _subItem("title")
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: IconButton(
+                    icon: Icon(Icons.home),
+                    iconSize: 22,
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: RaisedButton(
+                    child: Text("haha"),
+                    textColor: Colors.red,
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: PopupMenuButton(
+                      child: Text("abc"),
+                      tooltip: "长按提示",
+                      initialValue: "hot",
+                      itemBuilder: (BuildContext context) {
+                        return <PopupMenuItem<String>>[
+                          PopupMenuItem<String>(
+                            child: Text("热度"),
+                            value: "hot",
+                          ),
+                          PopupMenuItem<String>(
+                            child: Text("最新"),
+                            value: "new",
+                          )
+                        ];
+                      },
+                      onSelected: (String action) {
+                        switch (action) {
+                          case "hot":
+                            print("热度");
+                            break;
+                          case "new":
+                            print("罪行");
+                            break;
+                        }
+                      },
+                      onCanceled: () {
+                        print("oncancel");
+                      }),
                 )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    margin: EdgeInsets.all(15),
+                    child: RaisedButton(
+                      child: Text("弹窗"),
+                      onPressed: () {
+                        SimpleDialog(
+                          title: Text("标题"),
+                          titlePadding: EdgeInsets.all(10),
+                          children: <Widget>[
+                            SimpleDialogOption(
+                              child: Text("第一行"),
+                              onPressed: () {
+                                print("111111111111");
+                              },
+                            ),
+                            SimpleDialogOption(
+                              child: Text("第二行"),
+                              onPressed: () {
+                                print("22222222222");
+                              },
+                            )
+                          ],
+                          contentPadding: EdgeInsets.all(10),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    margin: EdgeInsets.all(15),
+                    child: RaisedButton(
+                      child: Text("弹窗"),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    margin: EdgeInsets.all(15),
+                    child: RaisedButton(
+                      child: Text("弹窗"),
+                    ),
+                  ),
+                ),
               ],
             )
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _subItem(String title) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
+        child: RaisedButton(
+          child: Text(title),
         ),
       ),
     );
