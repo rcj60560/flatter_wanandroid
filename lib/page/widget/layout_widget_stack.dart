76c353b826ce3f7c44fa00ddcc5adb2c174c5035
rcj60560 <63560560>
2021-04-28 22:39:45 +0800

    IndexedStack 排列

--- lib/page/widget/layout_widget_stack.dart
+++ lib/page/widget/layout_widget_stack.dart
@@ -10,6 +10,7 @@ class LayoutWidgetStack extends StatefulWidget {
 
 class LayoutWidgetStackState extends State<LayoutWidgetStack> {
   int index = 0;
+  int _position = 0;
   List des = List.from([
     AlignmentDirectional.bottomStart,
     AlignmentDirectional.bottomCenter,
@@ -56,10 +57,88 @@ class LayoutWidgetStackState extends State<LayoutWidgetStack> {
                   height: 150,
                 ),
               ],
-            ))
+            )),
+            SizedBox(
+              width: double.infinity,
+              height: 10,
+              child: Container(
+                color: Colors.black12,
+              ),
+            ),
+            _buildIndexStack(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      _position = 0;
+                    });
+                  },
+                  child: Text("1"),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      _position = 1;
+                    });
+                  },
+                  child: Text("2"),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      _position = 2;
+                    });
+                  },
+                  child: Text("3"),
+                ),
+              ],
+            )
           ],
         ),
       ),
     );
+  }
+
+  _buildIndexStack() {
+    return IndexedStack(
+        index: _position,
+        alignment: AlignmentDirectional.center,
+        children: <Widget>[
+          Center(
+            child: Container(
+              width: 200,
+              height: 200,
+              color: Colors.red,
+              child: Icon(
+                Icons.face,
+                size: 60,
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              width: 200,
+              height: 200,
+              color: Colors.yellow,
+              child: Icon(
+                Icons.save_alt,
+                size: 60,
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              width: 200,
+              height: 200,
+              color: Colors.greenAccent,
+              child: Icon(
+                Icons.fiber_dvr,
+                size: 60,
+              ),
+            ),
+          ),
+        ]);
   }
 }
